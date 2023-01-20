13cf6d36f0ea642609bc71422f9e75fe2b0d4a73
sondre haram <sondreharam@gmail.com>
2023-01-20 12:25:05 +0100

    test 1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int sales = 95000;
+    double StateTax = 0.04;
+    double CountyTax = 0.02;
+    double StateTaxCalc = sales * StateTax;
+    double CountyTaxCalc = sales * CountyTax;
+
+    cout << "Total earnings in $" << sales << endl;
+    cout << "Total State Tax " << StateTaxCalc << endl;
+    cout << "Total County Tax " << CountyTaxCalc << endl;
+    cout << "Total earnings is " << sales - StateTaxCalc - CountyTaxCalc;
+
+    return 0;
+}
